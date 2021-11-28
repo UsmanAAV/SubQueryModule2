146bfde1adef29122b42c79f1b93082ba80bd841
Azamat <azamat.abdullin@ringcentral.com>
2021-11-29 00:36:52 +0300

    exercise1: done

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,42 +1,15 @@
-import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {UsmanObject} from "../types";
+import {SubstrateEvent} from "@subql/types";
+import {Account} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
-
-export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new UsmanObject with ID using block hash
-    let record = new UsmanObject(block.block.header.hash.toString());
-    //Record block number
-    record.blockNumber = block.block.header.number.toNumber();
-
-    logger.info('\nBlockNumber: ' + record.blockNumber);
-    logger.info('\nspecVersion: ' + block.specVersion);
-    logger.info('\ntimestamp: ' + block.timestamp);
-    logger.info('\nparentHash: ' + block.block.header.parentHash);
-    logger.info('\nstateRoot: ' + block.block.header.stateRoot);
-    logger.info('\nextrinsicsRoot: ' + block.block.header.extrinsicsRoot);
-    logger.info('\ndigest: ' + block.block.header.digest);
-
-    await record.save();
-}
-
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
-    const {event: {data: [from, _to, value]}} = event;
-    //Retrieve the record by its ID
-    const record = await UsmanObject.get(event.extrinsic.block.block.header.hash.toString());
-    record.account = from.toString();
-    //Big integer type Balance of a transfer event
-    record.value = (value as Balance).toBigInt();
+    const {event: {data: [account, balance]}} = event;
+    //Create a new Account entity with ID using block hash
+    let record = new
+    Account(event.extrinsic.block.block.header.hash.toString());
+    // Assign the Polkadot address to the account field
+    record.account = account.toString();
+    // Assign the balance to the balance field "type cast as Balance"
+    record.balance = (balance as Balance).toBigInt();
     await record.save();
 }
-
-export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await UsmanObject.get(extrinsic.block.block.header.hash.toString());
-    //Date type timestamp
-    record.timestamp = extrinsic.block.timestamp;
-    //Boolean tyep
-    record.success = true;
-    await record.save();
-}
-
-
