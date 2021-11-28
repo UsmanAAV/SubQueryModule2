64b3ef8f316f1080be4214786833f08ed75ebe06
Azamat <azamat.abdullin@ringcentral.com>
2021-11-28 23:53:37 +0300

    lesson2: changed graphQL schema

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -1,38 +1,38 @@
 import {SubstrateExtrinsic,SubstrateEvent,SubstrateBlock} from "@subql/types";
-import {StarterEntity} from "../types";
+import {UsmanObject} from "../types";
 import {Balance} from "@polkadot/types/interfaces";
 
 
 export async function handleBlock(block: SubstrateBlock): Promise<void> {
-    //Create a new starterEntity with ID using block hash
-    let record = new StarterEntity(block.block.header.hash.toString());
+    //Create a new UsmanObject with ID using block hash
+    let record = new UsmanObject(block.block.header.hash.toString());
     //Record block number
-    record.field1 = block.block.header.number.toNumber();
+    record.blockNumber = block.block.header.number.toNumber();
     await record.save();
 }
 
 export async function handleEvent(event: SubstrateEvent): Promise<void> {
     const {event: {data: [from, _to, value]}} = event;
     //Retrieve the record by its ID
-    const record = await StarterEntity.get(event.extrinsic.block.block.header.hash.toString());
-    record.field2 = from.toString();
+    const record = await UsmanObject.get(event.extrinsic.block.block.header.hash.toString());
+    record.account = from.toString();
     //Big integer type Balance of a transfer event
-    record.field3 = (value as Balance).toBigInt();
+    record.value = (value as Balance).toBigInt();
 
     logger.info('\nMODULE: ' + event.event.method);
     logger.info('\nMETHOD: ' + event.event.section);
-    logger.info('\nVALUE: ' + record.field3);
-    logger.info('\nFROM: ' + record.field2);
+    logger.info('\nVALUE: ' + record.value);
+    logger.info('\nFROM: ' + record.account);
 
     await record.save();
 }
 
 export async function handleCall(extrinsic: SubstrateExtrinsic): Promise<void> {
-    const record = await StarterEntity.get(extrinsic.block.block.header.hash.toString());
+    const record = await UsmanObject.get(extrinsic.block.block.header.hash.toString());
     //Date type timestamp
-    record.field4 = extrinsic.block.timestamp;
+    record.timestamp = extrinsic.block.timestamp;
     //Boolean tyep
-    record.field5 = true;
+    record.success = true;
     await record.save();
 }
 
